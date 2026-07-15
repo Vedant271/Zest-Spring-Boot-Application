46885a448b723a135f02812c2d576d02b06021c2
Vedant Karale <vedantkarale271@gmail.com>
2026-07-15 19:10:15 +0530

    Initial commit

---
+++ src/test/java/com/example/ZestSpringBootApplication/ZestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZestSpringBootApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
